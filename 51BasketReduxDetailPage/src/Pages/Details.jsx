c4d1aa839b7ce8ec73127fd420bf7c906cff617d
arzummammadova <arzuam-af106@code.edu.az>
2024-12-28 07:59:18 +0400

    addded task

--- 51BasketReduxDetailPage/src/Pages/Details.jsx
+++ 51BasketReduxDetailPage/src/Pages/Details.jsx
@@ -1,79 +1,77 @@
-import React, { useEffect } from 'react'
-import './Details.css'
-import { useParams } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import './Details.css';
+import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from '../redux/ProductSlice';
-import { addToBasket } from '../redux/basketSlice';
-import { ToastContainer } from 'react-toastify';
-import { minusBtn,plusBtn } from '../redux/basketSlice';
+import { addToBasket, minusBtn, plusBtn } from '../redux/basketSlice';
+import { ToastContainer, toast } from 'react-toastify';
 
 const Details = () => {
   const { id } = useParams();
+  const dispatch = useDispatch();
 
   const { products } = useSelector((state) => state.products);
+  const { value: basket } = useSelector((state) => state.basket);
 
-  const dispatch = useDispatch()
-  const findProduct = products.find((product) => product.id === id)
+  const product = products.find((product) => product.id ==id);
+  const basketProduct = basket.find((item) => item.id ==id);
 
   useEffect(() => {
-    // fetchUser();
     dispatch(getProducts());
   }, [dispatch]);
-  const AddToBasket=(product,e)=>{
+
+  const AddToBasket = (product, e) => {
     e.stopPropagation();
-    dispatch( addToBasket(product));
-    toast.success('Added to basket sucsessfully');
-  }
-  const incrementProductCount = (product) => {
-    dispatch(plusBtn({ id: product.id }))
-    // dispatch(incrementProductCount({ id: product.id, count: 1}))
+    dispatch(addToBasket(product));
+    toast.success('Added to basket successfully');
   }
 
-  const decremenetProductCount = (product) => {
-    dispatch(minusBtn({ id: product.id }))
-   
+  const incrementProductCount = (product) => {
+    dispatch(plusBtn({ id: product.id }));
   }
+
+  const decrementProductCount = (product) => {
+    dispatch(minusBtn({ id: product.id }));
+  }
+
   return (
-    <div >
+    <div>
       <div className="product-details">
         <div className="product-details__image">
-          <img src={findProduct?.images} alt="Product Image" className="product-details__image-img" />
+          <img src={product?.thumbnail} alt={product?.title} className="product-details__image-img" />
         </div>
 
         <div className="product-details__info">
-          <h1 className="product-details__title">{findProduct?.title}</h1>
-          <p className="product-details__description">
-            {findProduct?.description}
-          </p>
+          <h1 className="product-details__title">{product?.title}</h1>
+          <p className="product-details__description">{product?.description}</p>
 
           <div className="product-details__rating">
             <span className="product-details__rating-stars">★★★★☆</span>
-            <span className="product-details__rating-count">{findProduct?.rating}</span>
+            <span className="product-details__rating-count">({product?.rating})</span>
           </div>
 
-          <p className="product-details__price">Price: <span className="product-details__price-amount">{findProduct?.price} AZN</span></p>
+          <p className="product-details__price">Price: <span className="product-details__price-amount">{product?.price} AZN</span></p>
 
-          <button className="product-details__add-to-basket" onClick={(e)=>AddToBasket(findProduct,e)}>Add to Basket</button>
-          <div className="count-area" style={{marginTop:"40px"}} >
-          <button className="minus-btn" disabled={findProduct?.count === 0} onClick={() => { decremenetProductCount(findProduct) }}  >-</button>
-            <p style={{color:"black"}}>{findProduct?.count}</p>
-            <button className="plus-btn" onClick={() => { incrementProductCount(findProduct) }}>+</button>
+          <button className="product-details__add-to-basket" onClick={(e) => AddToBasket(product, e)}>Add to Basket</button>
+
+          <div className="count-area" style={{ marginTop: "40px" }}>
+            <button className="minus-btn" disabled={basketProduct?.count === 0} onClick={() => decrementProductCount(product)}>-</button>
+            <p style={{ color: "black" }}>{basketProduct?.count || 0}</p>
+            <button className="plus-btn" onClick={() => incrementProductCount(product)}>+</button>
           </div>
 
           <div className="product-details__extra-info">
             <h3 className="product-details__extra-info-title">Product Details:</h3>
             <ul className="product-details__extra-info-list">
-              <li className="product-details__extra-info-item"><strong>Category:</strong>{findProduct?.category}</li>
-              <li className="product-details__extra-info-item"><strong>Brand:</strong> {findProduct?.brand}</li>
-              {/* <li className="product-details__extra-info-item"><strong>Color:</strong> Black</li>
-          <li className="product-details__extra-info-item"><strong>Warranty:</strong> 1 Year</li> */}
+              <li className="product-details__extra-info-item"><strong>Category:</strong> {product?.category}</li>
+              <li className="product-details__extra-info-item"><strong>Brand:</strong> {product?.brand}</li>
             </ul>
           </div>
         </div>
-        <ToastContainer/>
       </div>
+      <ToastContainer />
     </div>
-  )
+  );
 }
 
-export default Details
+export default Details;
--- 51BasketReduxDetailPage/src/redux/detailsSlice.js
+++ 51BasketReduxDetailPage/src/redux/detailsSlice.js
@@ -1,35 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-
-
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    value: [],
-  };
+  value: [],
+};
 
+const basketSlice = createSlice({
+  name: "basket",
+  initialState,
+  reducers: {
+    addToBasket: (state, action) => {
+      const existingProduct = state.value.find(item => item.id === action.payload.id);
 
+      if (existingProduct) {
+        existingProduct.count += 1;
+      } else {
+        state.value.push({ ...action.payload, count: 1 });
+      }
+    },
+    
+    plusBtn: (state, action) => {
+      const existingProduct = state.value.find(item => item.id === action.payload.id);
+      if (existingProduct) {
+        existingProduct.count += 1;
+      }
+    },
 
-
-
-
-
-const  details=createSlice({
-    name:"detail",
-    initialState,
-    reducers:{
-        // addToBasket: (state, action) => {
-        //     const existingProduct = state.value.find(item => item.id === action.payload.id);
-      
-        //     if (existingProduct) {
-        //       existingProduct.count += 1;
-        //     } else {
-        //       state.value.push({ ...action.payload, count: 1 });
-        //     }
-        //   },
+    minusBtn: (state, action) => {
+      const existingProduct = state.value.find(item => item.id === action.payload.id);
+      if (existingProduct && existingProduct.count > 0) {
+        existingProduct.count -= 1;
+      }
     }
+  },
+});
 
-
-})
-
-// export const { addToBasket} = details.actions;
-export default details.reducer;
+export const { addToBasket, plusBtn, minusBtn } = basketSlice.actions;
+export default basketSlice.reducer;
